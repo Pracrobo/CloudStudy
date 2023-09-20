6273bfbd84b4117719c60fc2d8b1151d7b864991
Pracrobo <cpu180621@gmail.com>
2023-09-20 16:02:10 +0900

    commit : new file ifdemo.py

---
+++ 230919/ifdemo.py
@@ -0,0 +1,19 @@
+# # 조건문
+# avg = float(input("Enter a Average : "))
+# if 90 <= avg <= 100 : print("A")
+# elif 80 <= avg < 90 : print("B")
+# elif 70 <= avg < 80 : print("C")
+# elif 60 <= avg < 70 : print("D")
+# else : print("F")
+
+
+season = input("What season do you like(Eng) ?").lower()
+if season == "spring" :
+    print("봄이다")
+elif season == "summer" :
+    print("여름이닷")
+elif season == "fall":
+    print("가을~")
+else: 
+    print("윈터임")
+
